e02a7552189655c34e4c67ca2121f9977b9d8f39
!!Timothy <jostimian@gmail.com>
2026-04-10 18:50:38 +0800

    [perf] camera capture performance improvements

--- bebot_camera_capture.py
+++ bebot_camera_capture.py
@@ -5,7 +5,12 @@ import cv2 as cv
 
 class CameraCapture:
 	def __init__(self):
-		self.capturer = cv.VideoCapture(0)
+		self.capturer = cv.VideoCapture(0, cv.CAP_V4L2)
+		self.capturer.set(cv.CAP_PROP_BUFFERSIZE, 1)
+		self.capturer.set(cv.CAP_PROP_FPS, 25)
+		self.capturer.set(cv.CAP_PROP_FRAME_WIDTH, 320)
+		self.capturer.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
+		self.capturer.set(cv.CAP_PROP_FOURCC, 1196444237)
 
 	def getImageBytes(self):
 		ret, img = self.capturer.read()
